fa0fcdaa93f6092df58cc97b88337a81a15fe9b0
ReaperScythe13 <breonono13853@gmail.com>
2025-07-12 00:33:45 -0400

    Header finish
    Set up the layout for the header and styled everything inside.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,24 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
 export default function Header(){
     return (
         <header>
-            <nav>
+            <nav className="header">
                 <h2>BALL.CO</h2>
                 <NavLink to="/shop">Shop</NavLink>
                 <NavLink to="/new">New Arrivals</NavLink>
                 <NavLink to="/brand">Brand</NavLink>
+                <form className="searchBar" action="/submit" method="get">
+                    <button type="submit"> <FaSearch className="search" /> </button>
+                    <input type="search" placeholder="Check up"></input>
+                    
+                </form>
+                <FaShoppingCart className="shoppingCart" />
+                <FaUserAlt className="user"/>
             </nav>
         </header>
     )
