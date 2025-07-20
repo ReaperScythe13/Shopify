17e5dc00a2ab5a07de33bda23cafc8ce22a0a4ac
ReaperScythe13 <breonono13853@gmail.com>
2025-07-19 22:41:05 -0400

    Login page
    Set up the connection to the backend for the login page.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import Shop from "./pages/shop/Shop"
+import Login from "./pages/Login"
 
 function App(){
     return (
@@ -12,6 +13,7 @@ function App(){
                 <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="shop" element={<Shop />} />
+                <Route path="login" element={<Login />} />
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export default function Login(){
+    const [loginData, setLoginData] = React.useState({ username: "", password: ""})
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState()
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        setStatus("Submitting")
+        try { const response = await fetch("http://localhost:5003/auth/login", 
+            {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(loginData)
+            }
+        )
+        const data = response.json()
+        if(!response.ok){
+            throw{
+                message: data.message,
+                statusText: response.statusText,
+                status: response.status
+
+            }
+        }
+            localStorage.setItem('loggedin', true)
+      }catch(err){
+            setError(err)
+      }finally{
+            setStatus('idle')
+      }
+    }
+    function handleChange(e){
+        const { name, value } = e.target
+        setLoginData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+    return (
+        <div className='loginForm'>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type='username' 
+                    name='username'
+                    onChange={handleChange} 
+                    placeholder='Username' 
+                    value= {loginData.username}
+                />
+                <input
+                    type='password'
+                    name='password'
+                    onChange={handleChange}
+                    placeholder='*********'
+                    value={loginData.password}
+                />
+                <button disabled= {status === 'Submitting'}>
+                    {status === 'Submitting' ? 'Logging in' : 'Log in'}
+                </button>
+            </form>
+        </div>
+    )
+}
